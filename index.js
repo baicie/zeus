6aa9b9e7734fd30b2218ccf35b897284c09229ed
baicie <zl316546@gmail.com>
2025-04-08 15:27:35 +0800

    删除 jsx-runtime 模块及其相关的 TypeScript 声明文件，清理未使用的代码以优化项目结构。

--- index.js
+++
@@ -1,4 +0,0 @@
-// 导出 jsx-runtime
-exports.jsx = require('./jsx-runtime').jsx
-exports.jsxs = require('./jsx-runtime').jsxs
-exports.Fragment = require('./jsx-runtime').Fragment
--- jsx-runtime/index.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function jsx(type: any, props: any, key?: any): any
-export declare const jsxs: typeof jsx
-export declare const Fragment: unique symbol
